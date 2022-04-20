e5255ec235e88c9dfb26f9ebad28ddbefcbb9e9d
AdnanTurjoy <aturjoy4@gmail.com>
2022-04-20 16:18:25 +0600

    output

---
+++ JAVA/Destructor/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(10+20 +"Hello, World!");
+        System.out.println("Hello, World!"+10+20);
+        System.out.println(10*20 +"Hello, World!");
+        System.out.println("Hello, World!"+10*20);
+    }
+}
