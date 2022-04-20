74b7885ac326ce377db272ff8beb89e8b3b880c5
AdnanTurjoy <aturjoy4@gmail.com>
2022-04-20 17:47:39 +0600

    inheritance with contructor

--- JAVA/Inheritance/src/Programmer.java
+++ JAVA/Inheritance/src/Programmer.java
@@ -1,12 +1,27 @@
 class Employee{
     int salary=90000;
+    void msg(){
+        System.out.println("Employee");
+    }
+    Employee(){
+        System.out.println("Employee Contructor");
+    }
 }
 
 public class Programmer extends Employee{
     int bonus =1500;
+    void msg(){
+        System.out.println("Programmer");
+    }
+    Programmer(){
+        System.out.println("Programmer Contructor");
+    }
     public static void main(String[] args) throws Exception {
         Programmer p1 = new Programmer();
         System.out.println(p1.salary);
         System.out.println(p1.bonus);
+        p1.msg();
+        Employee p2 = new Employee();
+        p2.msg();
     }
 }
