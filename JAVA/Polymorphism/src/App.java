9aa5629f8b69ec0eead512e1dae82054bdc862cc
AdnanTurjoy <aturjoy4@gmail.com>
2022-04-21 00:22:14 +0600

    poly

--- JAVA/Polymorphism/src/App.java
+++ JAVA/Polymorphism/src/App.java
@@ -13,10 +13,14 @@ class IBBL extends Bank{
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Bank bank;               // onek gula k ak object dia call korte chaile evabe korte hobe
+        Bank bank;               // onek gula k ak object dia call korte chaile evabe korte hobe (Like Refference Variables)
         bank = new DBBL();
         System.out.println(bank.getString());
         bank = new IBBL();
         System.out.println(bank.getString());
+        bank = new Bank();
+        System.out.println(bank.getString());
+
+
     }
 }
