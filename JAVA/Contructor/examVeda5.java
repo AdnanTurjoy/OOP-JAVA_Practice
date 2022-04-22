2ce55f5e3197cf9d6f080144e3f8fa7b5b266911
AdnanTurjoy <aturjoy4@gmail.com>
2022-04-22 23:35:41 +0600

    need clarify?

--- JAVA/Contructor/examVeda5.java
+++ JAVA/Contructor/examVeda5.java
@@ -1,16 +1,25 @@
 class Mammal {
+    Mammal(){
+        System.out.println("MAMMAL");
+    }
     void eat(Mammal m) {
         System.out.println("Mammal eats food");
     }
 }
 
 class Cattle extends Mammal {
+    Cattle(){
+        System.out.println("CATTLE");
+    }
     void eat(Cattle c) {
         System.out.println("Cattle eats hay");
     }
 }
 
 class Horse extends Cattle {
+    Horse(){
+        System.out.println("HORSE");
+    }
     void eat(Horse h) {
         System.out.println("Horse eats hay");
     }
@@ -20,6 +29,6 @@ public class examVeda5 {
     public static void main(String[] args) {
         Mammal h = new Horse();
         Cattle c = new Horse();
-        c.eat(h);                 // exceptional
+        c.eat(h);                 
     }
 }
