ab26b708fdbc6ac06eb821f081162ec1b6f01dc8
Adnan Turjoy <aturjoy4@gmail.com>
2022-08-29 01:42:24 +0600

    Update examVeda2.java

--- JAVA/Contructor/examVeda2.java
+++ JAVA/Contructor/examVeda2.java
@@ -17,3 +17,8 @@ public class examVeda2 {
         a.method();
     }
 }
+
+/* Overriding in Java simply means that the particular method would be called based on the run time type of the object and
+not on the compile time type. But in the above case the methods are static which means access to them is always resolved 
+during compile time only using the compile time type information.
+Accessing them using object references is just an extra liberty given by the designers of Java. */
